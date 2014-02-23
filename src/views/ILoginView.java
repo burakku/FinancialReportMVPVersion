cf49c2bdb607f48d3df75f97c66bf95155f2fd49
Farong Cheng <cfr009@163.com>
2014-02-22 22:28:15 -0500

    comment on views

--- src/views/ILoginView.java
+++ src/views/ILoginView.java
@@ -1,8 +1,17 @@
 package views;
 
 public interface ILoginView {
+	/**
+	 * get user id from user's input
+	 */
 	String getUserid();
+	/**
+	 * get user password from user's input
+	 */
 	String getUserPassword();
+	/**
+	 * set display result text to show any error
+	 */
 	void setResultText(String text);
 	void goUserPage();
 }
--- src/views/IRegisterView.java
+++ src/views/IRegisterView.java
@@ -1,9 +1,21 @@
 package views;
 
 public interface IRegisterView {
+	/**
+	 * get user id from user's input
+	 */
 	String getUserid();
+	/**
+	 * get user password from user's input
+	 */
 	String getPassword();
+	/**
+	 * get user name from user's input
+	 */
 	String getName();
+	/**
+	 * set display result text to show any error
+	 */
 	void setRegisterText(String text);
 	void goUserPage();
 }
