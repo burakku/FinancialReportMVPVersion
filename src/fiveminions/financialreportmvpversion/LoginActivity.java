535490d5fc645de608fddd5bbf3d1288fa3559ed
hailin <hlin78@gatech.edu>
2014-02-22 23:12:46 -0500

    login activity updated
    login activity updated

--- src/fiveminions/financialreportmvpversion/LoginActivity.java
+++ src/fiveminions/financialreportmvpversion/LoginActivity.java
@@ -1,5 +1,60 @@
 package fiveminions.financialreportmvpversion;
 
-public class LoginActivity {
+import model.MemoryModel;
+import presenters.LoginPresenter;
+import views.ILoginView;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends Activity implements ILoginView {
+	
+	private LoginPresenter loginPresenter;
+	EditText userId, password;
+	TextView resultTxt;
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login); 
+		loginPresenter = new LoginPresenter(this, new MemoryModel());
+		
+		userId = (EditText) findViewById(R.id.usernameLog);
+		password = (EditText) findViewById(R.id.passwordLog);		
+	}
+	
+	public void onLoginCheckButtonClick(View v){
+		loginPresenter.onClick();
+	}
+
+	@Override
+	public String getUserid() {
+		// TODO Auto-generated method stub
+		return userId.toString();
+	}
+
+	@Override
+	public String getUserPassword() {
+		// TODO Auto-generated method stub
+		return password.toString();
+	}
+
+	@Override
+	public void setResultText(String text) {
+		// TODO Auto-generated method stub
+		resultTxt.setText(text);
+	}
+
+	@Override
+	public void goUserPage() {
+		// TODO Auto-generated method stub
+		Intent intent = new Intent(this, UserpageActivity.class );
+		startActivity(intent);		
+	}
 
 }
